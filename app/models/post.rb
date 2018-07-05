f44053df826a6701da653aad9b835d0b06ea7f74
github username <github email address>
2018-07-05 21:15:43 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { minimum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-fiction), message: "%{value} is not a valid category" }
+  validates :clickability
+
+
+  def clickability
+
+    array = ["Won't Believe", "Secret", "Top", "Guess"]
+
+    if self.title
+
+      if !array.any? {|item| self.title.include?(item)}
+        errors.add(:title, "You can do better!")
+        return false
+      end
+
+    else
+      false
+    end
+
+  end
+
+
+
+
+
 end
